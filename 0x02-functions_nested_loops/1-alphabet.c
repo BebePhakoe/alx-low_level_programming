7dbd0599e1e9099b87db6343acb2b6b65bdc2096
Beloved Phakoe <Beloved.phakoe@outlook.com>
2023-07-18 08:52:59 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; 1++)
+	{
+		putchar(i);
+	}
+	_putchar('\n');
+}
