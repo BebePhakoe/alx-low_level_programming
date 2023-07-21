b3b375aa91504964a5657594da3ddb396efdff5c
Beloved Phakoe <Beloved.phakoe@outlook.com>
2023-07-21 04:17:19 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - Entry point
+ * @a: First integer
+ * @b: Second integer
+ * Return: Always 0 (Success)
+ *
+ */
+
+int mul(int a, int b)
+{
+	int results = a * b;
+return (results);
+}
