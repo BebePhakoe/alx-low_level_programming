85e045901c5792165e96b27cbc1e77f879aed084
Beloved Phakoe <Beloved.phakoe@outlook.com>
2023-08-02 08:39:24 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@ int check_prime(int n, int i);
  * Return: integer value
  */
 
-int is_prime_number(int n);
+int is_prime_number(int n)
 {
 	return (check_prime(n, 1));
 }
@@ -21,7 +21,7 @@ int is_prime_number(int n);
  * Return : 1 for prime number 0 composite
  */
 
-int check_prime(int n, int i);
+int check_prime(int n, int i)
 {
 	if (n <= 1)
 		return (0);
