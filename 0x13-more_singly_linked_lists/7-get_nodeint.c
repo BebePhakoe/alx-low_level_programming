f6a2f9b2a4c18b0cd2c6c7e99ecc12595707aecf
Beloved Phakoe <Beloved.phakoe@outlook.com>
2023-08-29 03:23:47 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * *get_nodeint_at_index - Entry point
+ * @head: first node in the linked lists
+ * @index: index of the node to return
+ * Return: pointer to the node were looking for, or NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = head;
+
+	while (temp && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (temp ? temp : NULL);
+}
